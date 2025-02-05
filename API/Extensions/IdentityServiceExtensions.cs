f1b160842dece04974dc4632461a9bf4a7b7add9
Mine <headbang92@hotmail.com>
2025-02-06 02:24:21 +0300

    Add jwt token service

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,8 @@
+using System.Text;
+using API.Services;
 using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
 namespace API.Extensions
@@ -12,7 +16,21 @@ namespace API.Extensions
                 opt.Password.RequireNonAlphanumeric = false;
             })
             .AddEntityFrameworkStores<DataContext>();
-            services.AddAuthentication();
+
+             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key super secret key super secret key super secret key super secret key super secret key super secret key"));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt => 
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer =false,
+                    ValidateAudience =false
+
+                };
+            });
+            services.AddScoped<TokenService>();
             return services;
         }
     }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -25,7 +27,8 @@ if (app.Environment.IsDevelopment())
 app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
-
+//always first
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -36,7 +39,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
+    await Seed.SeedData(context, userManager);
 }
 
 catch (Exception ex)
