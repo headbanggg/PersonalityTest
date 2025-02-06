007d2de9ca4e74d847eb3d9619254e453a348be8
Mine <headbang92@hotmail.com>
2025-02-06 03:24:58 +0300

    Add register, get current user methods

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,15 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class RegisterDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
+        public string Password { get; set; }
+
+        [Required]
         public string Username { get; set; }
     }
 }
