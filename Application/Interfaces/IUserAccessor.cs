c3424c4400323a31793b92af0eb16f9b9cf41c48
Mine <headbang92@hotmail.com>
2025-02-09 23:37:07 +0300

    Update IUserAccessor.cs

--- Application/Interfaces/IUserAccessor.cs
+++ Application/Interfaces/IUserAccessor.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 
 namespace Application.Interfaces
 {
     public interface IUserAccessor
     {
         string GetUsername();
+        string GetUserId();
+        Task<AppUser> GetUserAsync();
     }
 }
