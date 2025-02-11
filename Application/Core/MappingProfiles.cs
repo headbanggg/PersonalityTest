fe4a39c0bd433c239dab4d91d16efdcd71f69ae3
Mine <headbang92@hotmail.com>
2025-02-11 21:40:01 +0300

    Update MappingProfiles.cs

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
+using Application.Profiles;
+using Application.Questions.DTOs;
+using Application.Tests.DTOs;
 using AutoMapper;
 using Domain;
 
@@ -11,7 +15,27 @@ namespace Application.Core
     {
         public MappingProfiles()
         {
+         
             CreateMap<Question, Question> ();
+            CreateMap<TestResult, TestResultDto> ();
+            CreateMap<AppUser, UserProfile> ()
+            .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.DisplayName))
+            .ForMember(d => d.ImageUrl, o => o.MapFrom(s => s.ImageUrl))
+            .ForMember(d => d.Id, o => o.MapFrom(s => s.Id));
+            
+            CreateMap<Test, TestDto> ();
+            CreateMap<Question, QuestionProfile> ()
+            .ForMember(d => d.Content, o => o.MapFrom(s => s.Content))
+            .ForMember(d => d.Index, o => o.MapFrom(s => s.Index))
+            .ForMember(d => d.Id, o => o.MapFrom(s => s.Id));
+
+            CreateMap<Question, QuestionDto> ()
+            .ForMember(d => d.Content, o => o.MapFrom(s => s.Content))
+            .ForMember(d => d.Id, o => o.MapFrom(s => s.Id))
+            .ForMember(d => d.Index, o => o.MapFrom(s => s.Index))
+            .ForMember(d => d.AnswerOptionsCount, o => o.MapFrom(s => s.Test.AnswerOptionsCount))
+            .ForMember(d => d.TestId, o => o.MapFrom(s => s.Test.Id))
+            .ForMember(d => d.TestName, o => o.MapFrom(s => s.Test.TestName));
         }
     }
 }
