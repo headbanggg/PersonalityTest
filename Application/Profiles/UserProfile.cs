856b42d55d005b639aa9f4cb1b04f831953851dc
Mine <headbang92@hotmail.com>
2025-02-11 21:40:12 +0300

    Create UserProfile.cs

---
+++ Application/Profiles/UserProfile.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class UserProfile
+    {
+        public string Id { get; set; }
+        public string? DisplayName { get; set; }
+        public string? ImageUrl { get; set; }
+
+    }
+}
