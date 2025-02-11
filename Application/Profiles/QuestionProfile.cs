c461aacb3c02f0711aaaf141ddd8b0b8e0ae1602
Mine <headbang92@hotmail.com>
2025-02-11 21:40:05 +0300

    Create QuestionProfile.cs

---
+++ Application/Profiles/QuestionProfile.cs
@@ -0,0 +1,9 @@
+namespace Application.Profiles
+{
+    public class QuestionProfile
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public int Index { get; set; }
+    }
+}
