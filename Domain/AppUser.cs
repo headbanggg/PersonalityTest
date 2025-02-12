decf35ea55f70d912f9e0bb28623f030e4a4f144
Mine <headbang92@hotmail.com>
2025-02-12 23:30:37 +0300

    Update AppUser.cs

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -4,9 +4,10 @@ namespace Domain
 {
     public class AppUser : IdentityUser
     {
-        public string DisplayName { get; set; }
-        public string Bio { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Bio { get; set; }
+        public string? ImageUrl { get; set; }
         public Personality Personality { get; set; }
-        public ICollection<TestResult> TestResults { get; set; }
+        public ICollection<TestResult> TestResults { get; set; } = [];
     }
 }
